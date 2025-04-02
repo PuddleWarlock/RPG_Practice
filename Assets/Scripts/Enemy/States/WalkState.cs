1aa7dd10f6fa9df52a14e8a33514488ea10fd274
Kovalik Alexander <67467547+k0vaaa@users.noreply.github.com>
2025-04-02 16:34:51 +0300

    EnemyHitEffect added + animations rework
    Generic -> Humanoid animation type + hit effect on melee enemy

---
+++ Assets/Scripts/Enemy/States/WalkState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy
+{
+    public class WalkState : StatesEnemyConst
+    {
+        public WalkState(EnemyController enemyController, EnemyAnimator animator, NavMeshAgent navMeshAgent) : base(enemyController, animator, navMeshAgent)
+        {
+        }
+
+        public override void Enter()
+        {
+            Debug.Log("Entering ENEMY WALK");
+            EnemyAnimator.WalkEvent();
+            NavMeshAgent.isStopped = false;
+        }
+
+        public override void Execute()
+        {
+        }
+
+        public override void Exit()
+        {
+            
+        }
+    }
+}
